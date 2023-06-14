19df285f130c9396676a7a0c1e8c6528be86f0a3
陈晓辉 <2749555982@qq.com>
2023-06-14 17:49:24 +0800

    complete

---
+++ src/store/modules/setting.ts
@@ -0,0 +1,13 @@
+
+import { defineStore } from 'pinia'
+
+let useLayOutSettingStore = defineStore('SettingStore', {
+  state: () => {
+    return {
+      isCollapse: false,
+      refsh: false,
+    }
+  },
+})
+
+export default useLayOutSettingStore
